7038a27047594bbb87eac329c502c1c105bd8481
carl-larson <clarson08@gmail.com>
2020-07-23 20:36:14 -0500

    third attempt to deploy

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
     return (
         <div className="App">
-            <h3>About</h3>
+            <h3>ABOUT</h3>
             <p>This is where I will tell you about myself.</p>
         </div>
     )
