bba4b8f87d02818c814cac5047dfc29fe5cbc6ba
carl-larson <clarson08@gmail.com>
2020-07-27 16:05:19 -0500

    Updates

---
+++ precache-manifest.bdbc0b9570c052ac51afe3e8ad99be26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91fcb0aa374054d4ed6b715b0d88b9e",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "b8acc08b6c31159079dc",
+    "url": "/myportfolio/static/css/main.8e449dab.chunk.css"
+  },
+  {
+    "revision": "ef121715a4daddde1327",
+    "url": "/myportfolio/static/js/2.393feee9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.393feee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8acc08b6c31159079dc",
+    "url": "/myportfolio/static/js/main.31191bd9.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  }
+]);
